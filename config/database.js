477151d999a8d0a29fd1b420aa621db8dd317a71
Kyle <kylejhalvorson@gmail.com>
2017-06-20 19:24:34 -0700

    more sleep required

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 require('sequelize-hierarchy')(Sequelize);
 
 if(CONFIG.database.options.uri) {
-  var sequelize = new Sequelize(CONFIG.database.uri, CONFIG.database.options)
+  var sequelize = new Sequelize(CONFIG.database.options.uri, CONFIG.database.options)
 } else {
   var sequelize = new Sequelize(CONFIG.database.name, CONFIG.database.username, CONFIG.database.password, CONFIG.database.options)
 }
