d4effc0065e759d0538dd785eabd95ba23b01de4
Kyle <kylejhalvorson@gmail.com>
2017-06-20 19:23:12 -0700

    correct config

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@
 const Sequelize = require('sequelize');
 require('sequelize-hierarchy')(Sequelize);
 
-if(CONFIG.database.uri) {
+if(CONFIG.database.options.uri) {
   var sequelize = new Sequelize(CONFIG.database.uri, CONFIG.database.options)
 } else {
   var sequelize = new Sequelize(CONFIG.database.name, CONFIG.database.username, CONFIG.database.password, CONFIG.database.options)
