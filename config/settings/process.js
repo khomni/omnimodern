d00ae6b994483d3ce667de7e7e94445e54376662
Kyle <kylejhalvorson@gmail.com>
2017-06-21 21:23:11 -0700

    cluster settings

--- config/settings/process.js
+++ config/settings/process.js
@@ -1,6 +1,10 @@
+'use strict';
+
+let cpus = require('os').cpus().length;
+
 module.exports = {
   default: {
-    threads: require('os').cpus().length,
+    threads: process.env.CLUSTER_MAX ? Math.min(cpus, Math.max(1, process.env.CLUSTER_MAX)) : cpus,
   },
   local: {
     threads: 2
